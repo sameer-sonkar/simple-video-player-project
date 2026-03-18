15bff3a30f2cbf6a9f67c779febd801c4df3c1d9
sameer <sksonkar850@gmail.com>
2026-03-18 15:54:14 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`app is listening to the port: ${process.env.PORT} `);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+});
 
 // import express from "express";
 // const app=express();
---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    error = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { ApiError };
